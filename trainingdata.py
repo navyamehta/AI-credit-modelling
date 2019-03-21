15477683a00c1ae6a75bec1ee5f67d90b345b2ff
navyamehta <45111560+navyamehta@users.noreply.github.com>
2019-03-21 17:06:47 -0400

    Create trainingdata.py

---
+++ trainingdata.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+
+def getall(name):
+	try:
+		cnx = mysql.connector.connect(user='sql9273861', password='eCvI714qdd',
+                              	host='sql9.freesqldatabase.com',
+                              	database='sql9273861')
+		cursor = cnx.cursor()
+		query = "SELECT * FROM `" + name + "`"
+		cursor.execute(query)
+		fullrow = []
+		partrow = []
+		for row in cursor:
+			fullrow.append(row)
+			partrow.append(row[1])
+		cursor.close()
+		cnx.close()
+		finalrow = [fullrow, partrow]
+		return finalrow
+	except Error as e:
+		print("Error while connecting to MySQL database", e)
