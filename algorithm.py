452a77047e0f8c03f94d37b61e7f2e3c53de70f7
navyamehta <45111560+navyamehta@users.noreply.github.com>
2019-03-22 12:37:26 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -3,5 +3,5 @@ import trainingdata
 
 temp = trainingdata.gettrainingdata()
 colling = ['id', 'credutil', 'paymenthist', 'informalinc', 'histlength', 'debtincomeratio', 'emplength', 'phoneusage']
-CreditData = pd.dataframe(temp, columns = colling)
+CreditData = pd.DataFrame(temp, columns = colling)
 print(CreditData)
