a9592c6214fb4c287c1cb6c77b349d97dd60d4f0
navyamehta <45111560+navyamehta@users.noreply.github.com>
2019-03-22 11:36:27 -0400

    Update trainingdata.py

--- trainingdata.py
+++ trainingdata.py
@@ -10,15 +10,16 @@ def gettrainingdata():
 		query = "SELECT * FROM `CREDIT_DATA`"
 		cursor.execute(query)
 		fullrow = []
-		partrow = []
 		for row in cursor:
 			fullrow.append(row)
-			partrow.append(row[1])
 		cursor.close()
 		cnx.close()
-		finalrow = [fullrow, partrow]
-		return finalrow
+		return fullrow
 	except Error as e:
 		print("Error while connecting to MySQL database", e)
 
-def addnewtrainingdata():
+def addnewtrainingdata(idnum, credutil, paymenthist, informalinc, histlength, debtincomeratio, emplength, phoneusage):
+	try:
+		cnx = mysql.connector.connect(user='sql9284623', password='m8QTBgHjDr',
+                              	host='sql9.freesqldatabase.com',
+                              	database='sql9284623')
