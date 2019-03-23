d08cd3981fb9710aed3808bdf4556e7e6a275e37
nt7nguye <45516852+nt7nguye@users.noreply.github.com>
2019-03-22 22:57:21 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,37 @@
+# Defining tools 
+
 import xgboost
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 
-# Loading data
+# Loading clean data
 
-def makearray(loan_amnt, emp_length, home_ownership, annual_inc, verification_status, loan_status, title, delinq_2yrs,
+clean_data_path = "clean_data.csv"
+clean_data = pd.read_csv(clean_data_path, engine="python")
+
+X = clean_data.drop(["loan_status", "int_rate", "Unnamed: 0"], axis = 1)
+
+hot_encoded_X = pd.get_dummies(X)
+y = clean_data.loan_status
+
+# split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(hot_encoded_X, y, test_size=0.3, random_state=1)
+
+# fit model no training data
+model = XGBClassifier(n_estimators = 150, learning_rate = 0.05)
+model.fit(X_train, y_train)
+
+# make predictions for test data
+# y_pred = model.predict(X_test)
+
+# evaluate accuracy of predictions
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+
+def makearray(loan_amnt, emp_length, home_ownership, annual_inc, verification_status, title, delinq_2yrs,
              fico_range_low, fico_range_high, pub_rec, revol_bal, revol_util, tot_cur_bal, chargeoff_within_12_mths,
              public_rec_bankruptcies, total_il_high_credit_limit):
        home_ownership_ANY = 0;
@@ -46,7 +71,7 @@ def makearray(loan_amnt, emp_length, home_ownership, annual_inc, verification_st
               title_Business = 1
        if (title == "CAR FINANCING"):
               title_Car = 1
-       if (title == "CREDIT CARD FINANCING"):
+       if (title == "CREDIT CARD REFINANCING"):
               title_cc = 1
        if (title == "DEBT CONSOLIDATION"):
               title_Debt = 1
@@ -67,28 +92,20 @@ def makearray(loan_amnt, emp_length, home_ownership, annual_inc, verification_st
        if (title == "VACATION"):
               title_vacation = 1
        return [loan_amnt, emp_length, annual_inc, delinq_2yrs, fico_range_low, fico_range_high, pub_rec, revol_bal, revol_util, 
-               tot_cur_bal, chargeoff_within_12_mths, pub_rec_bankruptcies, total_il_high_credit_limit, home_ownership_ANY, 
+               tot_cur_bal, chargeoff_within_12_mths, public_rec_bankruptcies, total_il_high_credit_limit, home_ownership_ANY, 
                home_ownership_MORTGAGE, home_ownership_OWN, home_ownership_RENT, verification_status_NotV,
                verification_status_SV, verification_status_V, title_Business, title_Car, title_cc, title_Debt, title_green,
                title_homeb, title_homei, title_major, title_medical, title_moving, title_other, title_vacation]
 
 
-def mlalg(loan_amnt, emp_length, home_ownership, annual_inc, verification_status, loan_status, title, delinq_2yrs,
+def mlalg(loan_amnt, emp_length, home_ownership, annual_inc, verification_status, title, delinq_2yrs,
              fico_range_low, fico_range_high, pub_rec, revol_bal, revol_util, tot_cur_bal, chargeoff_within_12_mths,
              public_rec_bankruptcies, total_il_high_credit_limit):
-       dataarr = makearray(loan_amnt, emp_length, home_ownership, annual_inc, verification_status, loan_status, title, delinq_2yrs,
+       dataarr = makearray(loan_amnt, emp_length, home_ownership, annual_inc, verification_status, title, delinq_2yrs,
                            fico_range_low, fico_range_high, pub_rec, revol_bal, revol_util, tot_cur_bal, chargeoff_within_12_mths,
                            public_rec_bankruptcies, total_il_high_credit_limit)
-       clean_data_path = "clean_data.csv"
-       clean_data = pd.read_csv(clean_data_path, engine="python")
-       X = clean_data.drop(["loan_status", "int_rate"], axis = 1)
-       hot_encoded_X = pd.get_dummies(X)
-       y = clean_data.loan_status
-       X_train = hot_encoded_X
-       y_train = y
-       model = XGBClassifier(n_estimators = 150, learning_rate = 0.05)
-       model.fit(X_train, y_train)
-       X_test = pd.DataFrame(datarr, columns = ['Unnamed: 0', 'loan_amnt', 'emp_length', 'annual_inc', 'delinq_2yrs',
+        
+       X_test = pd.DataFrame([dataarr], columns = ['loan_amnt', 'emp_length', 'annual_inc', 'delinq_2yrs',
                                                 'fico_range_low', 'fico_range_high', 'pub_rec', 'revol_bal',
                                                 'revol_util', 'tot_cur_bal', 'chargeoff_within_12_mths',
                                                 'pub_rec_bankruptcies', 'total_il_high_credit_limit',
@@ -99,5 +116,11 @@ def mlalg(loan_amnt, emp_length, home_ownership, annual_inc, verification_status
                                                 'title_Debt consolidation','title_Green loan', 'title_Home buying', 
                                                 'title_Home improvement','title_Major purchase', 'title_Medical expenses',
                                                 'title_Moving and relocation', 'title_Other', 'title_Vacation'])
+       
        y_pred = model.predict_proba(X_test) * 100
-       return y_pred
+       result = y_pred[:, 0][0]
+       return result
+    
+""" Sample Call
+a = mlalg(12000, 0, "RENT", 30000, "NOT VERIFIED", "CREDIT CARD REFINANCING", 0, 665, 669, 3, 8862, 38.2, 8862, 0, 0, 0)
+print(a)"""
